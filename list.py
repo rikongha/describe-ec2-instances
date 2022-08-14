1f36d1c611c3c715768a1ebb1b1ffc8d6ac96ede
Redarokim Ikongha <rikongha@gmail.com>
2022-08-14 23:20:39 +0100

    refactors script for easy switch between environments

--- list.py
+++ list.py
@@ -1,20 +1,99 @@
 import boto3
-from tabulate import tabulate
-from dotenv import load_dotenv
+import logging
 import os
 
-# Load credentials
-load_dotenv()
-access_key = os.getenv('ACCESS_KEY')
-secret_key = os.getenv('SECRET_KEY')
-region_name = os.getenv('REGION')
 
-ec2_client = boto3.client('ec2', aws_access_key_id=access_key, aws_secret_access_key=secret_key, region_name='eu-central-1')
+ec2_instances = []
+volume_sizes=[]
+
+# Get capacity of attached volumes in GB
+def get_attached_volume_size(instances, ec2):
+    describedVolumes = ec2.describe_volumes(
+        Filters=[{'Name':'attachment.instance-id',
+        'Values':[instances['InstanceId']]}]
+    )
+
+    volumeSize = 0
+    GiB_TO_GB = 1.073741824
+    for volume in (describedVolumes["Volumes"]):
+        volumeSize += (volume["Size"])
+
+    return volumeSize * GiB_TO_GB
+
+# Helper function used to validate input
+def check_defined(reference, reference_name):
+    if not reference:
+        raise Exception('Error: ', reference_name, 'is not defined')
+    return reference
+
+def get_server_by_name(client, name):
+    return client.describe_instances(
+        Filters=[{
+            'Name': 'tag:Name',
+            'Values': [name]
+        }]
+    )
+
+def get_all_servers(client):
+    return client.describe_instances()
+
+def sort_by_size():
+    return #TODO sort results
 
 
-# def find_server(filter):
+def lambda_handler(event, context):
+    logging.basicConfig(level=logging.ERROR)
 
-#def get_volumes:
+    try:
+        logging.info("-----------EC2 Instance Description------------")
 
-#
+        # check_defined()
+        region = 'eu-central-1'
+        environment = os.getenv('ENVIRONMENT')
 
+        if 'local' ==  environment:
+            awsProfile = os.getenv('PROFILE')
+            if not awsProfile: 
+                awsProfile = 'default'
+            session = boto3.Session(profile_name=awsProfile, region_name=region)
+        else:  
+            session = boto3.Session(region_name=region)
+            check_defined(event, 'event')
+            # TODO get input from events and call appropriate fn
+
+
+        ec2_client = session.client('ec2')
+
+        reservations = get_server_by_name(ec2_client, 'jenkins')
+
+
+        for reservation in reservations["Reservations"]:
+            for instance in reservation['Instances']:
+                volumeSize = get_attached_volume_size(instance, ec2_client)
+                volume_sizes.append(volumeSize)
+
+                ec2_instances.append({
+                    'InstanceId': instance.get('InstanceId', ''),
+                    'InstanceType': instance.get('InstanceType', ''),
+                    'State': instance['State']['Name'],
+                    'PublicIp': instance.get('PublicIpAddress', ''),
+                    'PrivateIp': instance.get('PrivateIpAddress', ''),
+                    'SubnetId': instance.get('SubnetId', ''),
+                    'VpcId': instance.get('VpcId', ''),
+                    'volume.size': volumeSize           
+                    })
+                    
+        # TODO Print result to console  in tabular format
+        header = ['instance-id', 'instance-type', 'status', 'private-ip', 'PublicIp', 
+            'public-ip', 'total-size-ebs-volumes']
+        
+        total_of_all_server_volumes = sum(volume_sizes)
+        print(f'Total volume across all servers: {total_of_all_server_volumes:.2f} GB')
+
+    except Exception as e:
+        logging.exception(
+            'Task failed with exception: {}'.format(e)
+        )
+
+if __name__ == '__main__': 
+    lambda_handler(None, None)
