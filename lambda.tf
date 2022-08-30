24b8ca756943e75d7994e4333cfd3eee2fd00fd2
Redarokim Ikongha <rikongha@gmail.com>
2022-08-30 08:58:54 +0100

    separated dependencies to lambda layers

--- lambda.tf
+++ lambda.tf
@@ -1,3 +1,9 @@
+resource "aws_lambda_layer_version" "lambda_layer" {
+  filename   = "python.zip"
+  layer_name = "lambda_layer_python"
+  compatible_runtimes = ["python3.9"]
+}
+
 resource "aws_lambda_function" "ec2lambda" {
     filename = "describe_ec2_lambda.zip"
     function_name = "list"
