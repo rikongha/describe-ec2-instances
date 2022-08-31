b5e4446858820799c4b15cfb66c1bb02da6cd3f5
Redarokim Ikongha <rikongha@gmail.com>
2022-08-31 10:32:57 +0100

    fixed formatting

--- main.tf
+++ main.tf
@@ -3,12 +3,13 @@ terraform {
 
     required_providers {
         aws = {
-        source  = "hashicorp/aws"
-        version = "~> 4.0"
+            source  = "hashicorp/aws"
+            version = "~> 4.0"
         }
     }
 }
 
 provider "aws" {
+    profile = "cocus"
     region = "eu-central-1"
 }
